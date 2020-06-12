fab833c18c5522abe3b597f81df77663961859e2
Christian Brunet <cbrunet1@medphysnt.ucl.ac.uk>
2020-06-12 09:03:17 +0100

    Refined version of the line profile interface. Still needs tidying up.

--- scripts/ColorMapperV3.py
+++ scripts/ColorMapperV3.py
@@ -4,6 +4,7 @@
 # Might want this for line profiles?
 from scipy.ndimage import map_coordinates
 import math
+import random
 
 # pandas and numpy for data manipulation
 import pandas as pd
@@ -15,7 +16,7 @@ from datetime import date
 # functions from bokeh
 from bokeh.plotting import figure
 from bokeh.models import (CategoricalColorMapper, HoverTool, BoxZoomTool,
-	PanTool, WheelZoomTool, ResetTool, ColumnDataSource, Panel,
+	PanTool, WheelZoomTool, ResetTool, ColumnDataSource, Panel, CrosshairTool,
 	FuncTickFormatter, SingleIntervalTicker, LinearAxis, CustomJS,
 	DatetimeTickFormatter, BasicTickFormatter, NumeralTickFormatter, Arrow,
 	NormalHead, OpenHead, VeeHead, Label, PointDrawTool, Range1d)
@@ -52,15 +53,11 @@ class SpotParameters:
 
 def ColorMapper(conn):
 
-	output_file("ColorMapper.html") #????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
-
 	# Quick plot of charmander image to check file orientations are done
-	# correctly.
-
+	# correctly. Also later for fun.
 	file_charmander = 'O:\\protons\\Work in Progress\\Christian\\Python\\Graphing Code\\CB Version\\New Ideas\\Charmander.png'
 	arr_charmander=np.array(Image.open(file_charmander))
 	arr_charmander=np.flipud(arr_charmander)
-
 	p_charmander = figure()
 	p_charmander.plot_height = 300
 	p_charmander.plot_width = 300
@@ -68,90 +65,150 @@ def ColorMapper(conn):
 
 
 
+	############################################################################
+	############################ READ IN THE IMAGE #############################
+
 	# File address for where bmp image is
 	file1 = 'O:\\protons\\Dosimetry_and_QA\\TestData\\LOGOS Example Data\\Bmp Images\\00000001.bmp'
-
 	# Read in image and turn into an array
 	arr1=np.array(Image.open(file1))
 	# Flip the array because Bokeh reads the array in from the bottom left corner
 	# but the image
 	arr1=np.flipud(arr1)
-	max1 = (np.amax(arr1))
+	# Get the width and height of the array. Annoyingly pixels are accessed by
+	# (y,x) coordinates.
 	(dh1, dw1) = arr1.shape
-	print(arr1)
-	print(arr1.shape)
-	print('\nHeight array= ' + str(dh1))
-	print('\nWidth of array= ' +str(dw1))
-	print('\nMax value in array = ' +str(max1))
-	print(arr1[600, 800])
+
+	############################################################################
+	############################################################################
 
 
 
 
 
-	# Set some values for the start and end x and y
-	x_prof_start = 600
-	x_prof_end = 600
-	y_prof_start = 200
-	y_prof_end = 1000
+	############################################################################
+	######################## SET SOME STARTING VALUES ##########################
 
-	# Calculate how long the line is
+	# Define the data for each spot. In this case the expected coordinates, then
+	# the viewing range for the windows, and then finally the measured positions
+	# At the moment these measured positions are just generated randomly but
+	# will eventually be inputs from CG's code.
+	viewing_range = 40
+	spot_tl = SpotParameters(600, 800, viewing_range, random.gauss(600, 5), random.gauss(800, 5))
+	spot_tc = SpotParameters(800, 800, viewing_range, random.gauss(800, 5), random.gauss(800, 5))
+	spot_tr = SpotParameters(1000, 800, viewing_range, random.gauss(1000, 5), random.gauss(800, 5))
+	spot_ml = SpotParameters(600, 600, viewing_range, random.gauss(600, 5), random.gauss(600, 5))
+	spot_mc = SpotParameters(800, 600, viewing_range, random.gauss(800, 5), random.gauss(600, 5))
+	spot_mr = SpotParameters(1000, 600, viewing_range, random.gauss(1000, 5), random.gauss(600, 5))
+	spot_bl = SpotParameters(600, 400, viewing_range, random.gauss(600, 5), random.gauss(400, 5))
+	spot_bc = SpotParameters(800, 400, viewing_range, random.gauss(800, 5), random.gauss(400, 5))
+	spot_br = SpotParameters(1000, 400, viewing_range, random.gauss(1000, 5), random.gauss(400, 5))
+
+	# Set some values for the start and end x and y for the line profile we'll
+ 	# make later. (For simplicity we're going to plot a +ve diagonal over the
+	# topleft (tl) spot)
+	x_prof_start = 570
+	x_prof_end = 630
+	y_prof_start = 770
+	y_prof_end = 830
+
+	############################################################################
+	############################################################################
+
+
+
+
+
+	############################################################################
+	######################### WORK OUT A LINE PROFILE ##########################
+
+	# This should work out the pixel values along an arbitary line profile.
+	# NB: It does some sort of interpolation between pixels that should probably
+	# be looked at in some more detail.
+
+	# Calculate how long the line is (hypotenuse eqn.). This will give the
+	# length in pixels.
 	prof_length = math.sqrt((x_prof_start-x_prof_end)**2
 		+ (y_prof_start-y_prof_end)**2)
-	# Going to sample every 0.1 pixels which should be plenty
+	# Going to sample every 0.1 pixels which should be plenty. (Turn it into an
+	# integer to make sure its a whole number)
 	prof_sample = int(prof_length/0.1)
-	# Create a list of the x and y coordinates
+	# Create a list of the x and y coordinates by making an evenly spaced list
+	# according to the decided number of points.
 	x_prof_sample = np.linspace(x_prof_start, x_prof_end, prof_sample)
 	y_prof_sample = np.linspace(y_prof_start, y_prof_end, prof_sample)
-	# Map coordinates is like an interpolation thing. Come back to this later?
+	# Create a list of pixel values at these coordinates (interpolating where
+	# needed). map_coordinates an interpolation thing. Come back to this later?
 	z_prof_sample = map_coordinates(arr1, np.vstack((y_prof_sample, x_prof_sample)))
-	# Normalise to the max value in the profile
+	# Normalise to the max value in the profile (Easier to visualise things like
+	# FWHM etc.)
 	z_prof_sample = z_prof_sample*(100/(max(z_prof_sample)))
-	# Make it into a dictionary
+	# Make it into a dictionary where 'x' is the 'length' dimension and 'y' is
+	# the pixel value. Currently this is not meaningful so need to fill a pixel
+	# size in later?
 	dict_prof = {	'x': list(range(0, len(z_prof_sample))),
 					'y': z_prof_sample}
+	# Turn it into a dataframe
 	df_prof = pd.DataFrame(dict_prof)
+	# Turn it into a column data source
 	src_prof = ColumnDataSource(df_prof.to_dict(orient='list'))
+	# Create the profile plot
 	p_prof = figure()
 	p_prof.line(source=src_prof, x='x', y='y')
+	hover = HoverTool(	tooltips = [('X-Axis', '@x'),
+									('Y-Axis', '@y')],
+						mode = 'hline',
+						line_policy = 'nearest')
+	cross = CrosshairTool()
+	p_prof.add_tools(hover, cross)
 	p_prof.plot_height = 300
 	p_prof.plot_width = 600
-
+	# While here create a column data source (via dictionary and dataframe) that
+	# is the two points that define the line profile. Will use this later to
+	# display the line profile over the images.
 	dict_prof_points = {'x': [x_prof_start, x_prof_end],
 						'y': [y_prof_start, y_prof_end]}
 	df_prof_points = pd.DataFrame(dict_prof_points)
 	src_prof_points = ColumnDataSource(df_prof_points.to_dict(orient='list'))
+	# Going to add a datatable to display the position of these two points as
+	# well. (Apparently this is done by make a datatable widget?)
+	columns_prof_points = [TableColumn(field="x", title="x-position", width = 100),
+           TableColumn(field='y', title='y-position', width = 100)]
+	datatable_prof_points = DataTable(source=src_prof_points,
+		columns=columns_prof_points, width=600, height=100, editable=True,
+		selectable='checkbox')
+
+
+
+	############################################################################
+	############################################################################
 
 
 
 
 
-	# Create the main image
+	############################################################################
+	########################### CREATE THE MAIN PLOT ###########################
+
+	# The main plot is the one that's going to display the whole image.
+
+	# Create the main image with some basic tools
 	p_main = figure(tools=[PanTool(), WheelZoomTool(),
 		BoxZoomTool(match_aspect=True), ResetTool()],
 		tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],)
 	p_main.plot_height = round(dh1/2)
 	p_main.plot_width = round(dw1/2)
+	# NB: This needs rewriting to the form of a column data source.
 	p_main.image(image=[arr1], x=0, y=0, dw=dw1, dh=dh1, palette="Spectral11", level="image")
-
-
-	# Add a the line profile to the main image.
+	# Add the line profile to the main image.
 	p_main.line(source=src_prof_points, x='x', y='y', line_width=2,
 		color='black')
+	# Add circle glyphs at either end of the line. Do it in this way so that
+	# they can be added
 	c1 = p_main.circle(source=src_prof_points, x='x', y='y', color='black',
 		fill_alpha=0.5, size=12)
 	p_main.add_tools(PointDrawTool(renderers=[c1], num_objects=2))
 
-	# Define the expected position of the spots
-	spot_tl = SpotParameters(600, 800, 40, 605, 798)
-	spot_tc = SpotParameters(800, 800, 40, 803.5, 798)
-	spot_tr = SpotParameters(1000, 800, 40, 997.5, 795)
-	spot_ml = SpotParameters(600, 600, 40, 602.1, 585)
-	spot_mc = SpotParameters(800, 600, 40, 605, 798)
-	spot_mr = SpotParameters(1000, 600, 40, 605, 798)
-	spot_bl = SpotParameters(600, 400, 40, 605, 798)
-	spot_bc = SpotParameters(800, 400, 40, 605, 798)
-	spot_br = SpotParameters(1000, 400, 40, 605, 798)
 
 
 	spot_parameters = [spot_tl, spot_tc, spot_tr, spot_ml, spot_mc, spot_mr,
@@ -260,7 +317,7 @@ def ColorMapper(conn):
 	row2 = row(p_ml, p_mc, p_mr)
 	row3 = row(p_bl, p_bc, p_br)
 	column1 = column(row1, row2, row3)
-	column2 = column(row(p_prof, p_charmander), p_main)
+	column2 = column(row(p_prof, p_charmander), datatable_prof_points, p_main)
 	layout = row(column1, column2)
 
 
@@ -302,6 +359,10 @@ def ColorMapper(conn):
 
 		x_prof_start, x_prof_end = dict_prof_points['x']
 		y_prof_start, y_prof_end = dict_prof_points['y']
+		x_prof_start = float(x_prof_start)
+		x_prof_end = float(x_prof_end)
+		y_prof_start = float(y_prof_start)
+		y_prof_end = float(y_prof_end)
 
 		# Calculate how long the line is
 		prof_length = math.sqrt((x_prof_start-x_prof_end)**2
@@ -325,6 +386,7 @@ def ColorMapper(conn):
 		return
 
 	src_prof_points.on_change('data', callback_prof)
+	datatable_prof_points.on_change('source', callback_prof)
 
 
 
