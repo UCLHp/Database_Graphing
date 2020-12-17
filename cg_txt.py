d3bfa0e05c0a9f2f8393f7e9b102bdbee08954b1
callumgillies <callumgillies@nhs.net>
2020-12-17 06:31:08 +0000

    Plot successful with legend toggle, whisker commented - not working with legend toggle

--- cg_txt.py
+++ cg_txt.py
@@ -1,5 +1,6 @@
+from bokeh.palettes import Spectral4
 from bokeh.plotting import figure, show, ColumnDataSource
-from bokeh.models import CheckboxGroup, CustomJS
+from bokeh.models import Whisker
 import pandas as pd
 import pypyodbc
 import json
@@ -10,11 +11,12 @@ with open('config.json') as config_file:
 
 DATABASE_DIR = config["DATABASE_DIR"]
 
+
 def db_connect(DATABASE_DIR, *, pswrd=''):
     '''
     Function to connect to connect to the access database at location defined
-    by the DATABASE_DIR input. Connection will fail if password is required
-    but not supplied
+    in config file. Connection will fail if password is required but not
+    supplied
     '''
 
     if pswrd != '':
@@ -27,38 +29,43 @@ def db_connect(DATABASE_DIR, *, pswrd=''):
             )
     return conn
 
+
 conn = db_connect(DATABASE_DIR)
 
 coils_df = pd.read_sql('select [Coil_name] from [MRI_Coils]', conn)
 coils = list(coils_df['coil_name'])
+
 qa_results = pd.read_sql('select * from [MRI_Coils_Check]', conn)
 
-source = ColumnDataSource(qa_results)
-
-coil_select_box = CheckboxGroup(labels=coils, active =[0,1])
-coil_select_box.js_on_click(CustomJS(code="""
-    console.log('coil_select_box: active=' + this.active, this.toString())
-    """))
-show(coil_select_box)
-
-exit()
-
 p = figure(
-    title = 'Example',
-    x_axis_label='X Axis',
+    title='MRI Coil QA Results',
+    x_axis_label='Date',
     x_axis_type='datetime',
-    y_axis_label='Y Axis'
+    y_axis_label='SNR'
 )
 
+for name, color in zip([f'{i}' for i in coils], Spectral4):
+    df = pd.DataFrame(qa_results).where(qa_results['coil'] == name)
+    df['adate'] = pd.to_datetime(df['adate'])
+    base = df['adate']
+    lower = list(df['snr']-df['snr_std'])
+    upper = list(df['snr']+df['snr_std'])
+    error = ColumnDataSource(data=dict(base=base, lower=lower, upper=upper))
 
+    # p.add_layout(Whisker(source=error,
+    #                      base='base',
+    #                      lower='lower',
+    #                      upper='upper'
+    #                      )
+    #              )
+    p.scatter(df['adate'], df['snr'],
+              color=color,
+              alpha=0.8,
+              legend_label=name,
+              visible=False
+              )
 
-
-
-
-
-
-
-
-p.scatter(x='adate', y='snr', legend='Test', source=source)
+p.legend.location = "top_left"
+p.legend.click_policy = "hide"
 
 show(p)
