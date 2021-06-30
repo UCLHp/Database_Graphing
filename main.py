b7eb0ed8094d1451b1ac7d165da2650dad848e74
mariaboutros <maria.boutros@nhs.net>
2021-06-30 10:57:26 +0100

    Added Electron Output

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ from scripts.ElectronEnergy import Electron_Energy_Graph
 from scripts.JawTravel import JawTravel
 from scripts.FlexitronOutput import Flexitron_Output_Graph
 from scripts.Sym import Sym_Graph
+from scripts.ElectronOutput import Electron_Output_Graph
 
 ###### Start of patch!
 ###### Need a fix for running the Tornado Server in Python 3.8 on Windows. This
@@ -97,9 +98,10 @@ def produce_doc(doc):
         # Create each tab by running the relevant scripts
         tab1 = Photon_Output_Graph(conn)
         tab2 = Electron_Energy_Graph(conn)
-        tab3 = Sym_Graph(conn)
+        tab3 = Electron_Output_Graph(conn)
+        tab4 = Sym_Graph(conn)
         # Put all the tabs into one application
-        tabs = Tabs(tabs = [tab1, tab2, tab3])
+        tabs = Tabs(tabs = [tab1, tab2, tab3, tab4])
     elif choice == 'Proton':
         tab1 = Photon_Output_Graph(conn)
         tab2 = Flexitron_Output_Graph(conn)
---
+++ scripts/ElectronOutput.py
@@ -0,0 +1,503 @@
+'''
+Scripts for plotting from the Electron Output Table
+
+'''
+
+
+# pandas and numpy for data manipulation
+import types
+import pandas as pd
+import numpy as np
+from easygui import buttonbox, msgbox
+from datetime import date, timedelta
+
+from bokeh.plotting import figure
+from bokeh.models import (CategoricalColorMapper, HoverTool, BoxZoomTool,
+						  ColumnDataSource, Panel,
+						  FuncTickFormatter, SingleIntervalTicker, LinearAxis,
+						  CustomJS, DatetimeTickFormatter, BasicTickFormatter,
+						  NumeralTickFormatter, Range1d, Div)
+from bokeh.models.widgets import (CheckboxGroup, Slider, RangeSlider,
+								  Tabs, CheckboxButtonGroup, Dropdown,
+								  TableColumn, DataTable, Select,
+								  DateRangeSlider, Button)
+from bokeh.layouts import column, row, WidgetBox, layout
+from bokeh.palettes import turbo, Colorblind
+import bokeh.colors
+from bokeh.io import output_file, show
+from bokeh.transform import factor_cmap, factor_mark
+from bokeh.models.callbacks import CustomJS
+
+from scripts.Universal import (	Create_Select_Axis, Create_Select_Legend,
+								Create_Range_Sliders, Update_Range_Sliders,
+								Update_Range_Sliders_2,
+								Create_Checkbox_Legend, Define_Plot_Parameters,
+								Update_HoverTool, Create_Legend, Make_Dataset,
+								Make_Dataset_Tolerance,
+								Create_Checkbox_HoverTool)
+
+
+def create_df(sql, conn):
+
+	'''
+	Takes a connection to an MS Access database and pulls information from a
+	table in that database into a dataframe using an SQL Query
+	'''
+
+	# Read from database into dataframe
+	df = pd.read_sql(sql, conn)
+
+	# Delete empty rows where the data is very important to have
+	df = df.dropna(subset=['protocol id'], how='any')
+	df = df.dropna(subset=['energy'], how='any')
+
+	# Get adate and machine name from the protocol id field
+	df_left = df['protocol id'].str.partition(sep = '_')
+	df_right = df['protocol id'].str.rpartition(sep = '_')
+	df.loc[:, 'adate'] = df_left[0]
+	df.loc[:, 'machinename'] = df_right[2]
+	df.loc[:, 'adate'] = pd.to_datetime(df.loc[:,'adate'], dayfirst=True)
+
+	# Drop any rows that aren't related to the Truebeams
+	df = df[df['machinename'].isin(['TrueBeam B', 'TrueBeam C'])]
+
+	# Drop any columns where there is no data
+	df = df.dropna(axis='columns', how='all')
+
+	return df
+
+
+def special_tolerance(color_to_plot, x_data1, y_data1, Sub_df1, df_tol1_ratios):
+
+	'''
+	Special function to cope with the 2 depth ratios because multiple energies can be
+	dislayed at once with different tolerances per energy.
+
+	Created fairly ad hoc to work with format of df_tol1_ratios and produce and appropriate
+	sub_df_tol1_ratios
+	'''
+
+	energy_list = ['6MeV', '9MeV', '12MeV', '15MeV']
+	data = {}
+
+	if (x_data1 != 'adate') or ((y_data1 != 'ratio: depth 2/depth 1') and (y_data1 != 'graph abs diff in ratios')):
+		for x in range(0, len(energy_list)):
+			data.update({'x_' + energy_list[x]: [Sub_df1['x'].max(),
+						Sub_df1['x'].max()],
+					'y_low_' + energy_list[x]: [Sub_df1['y'].max(),
+						Sub_df1['y'].max()],
+					'y_high_' + energy_list[x]: [Sub_df1['y'].max(),
+						Sub_df1['y'].max()]})
+	elif y_data1 == 'ratio: depth 2/depth 1':
+		# Get a list of the column headers
+		headers1 = df_tol1_ratios.columns.values.tolist()
+		# Check if the xdata is what is in the df_tol1 as the x_axis (if not no
+		# point plotting tolerances as all tolerances are vs this column).
+		max_x = Sub_df1['x'].max() + pd.DateOffset(weeks = 2)
+		min_x = Sub_df1['x'].min() + pd.DateOffset(weeks = -2)
+
+		for x in range(0, len(energy_list)):
+			if energy_list[x] in color_to_plot:
+				data.update({'x_' + energy_list[x]: [min_x, max_x],
+						'y_low_' + energy_list[x]:
+							[df_tol1_ratios['ratios_' + energy_list[x]][0],
+							df_tol1_ratios['ratios_' + energy_list[x]][0]],
+						'y_high_' + energy_list[x]:
+							[df_tol1_ratios['ratios_' + energy_list[x]][1],
+							df_tol1_ratios['ratios_' + energy_list[x]][1]]})
+			else:
+				data.update({'x_' + energy_list[x]: [Sub_df1['x'].max(),
+							Sub_df1['x'].max()],
+						'y_low_' + energy_list[x]: [Sub_df1['y'].max(),
+							Sub_df1['y'].max()],
+						'y_high_' + energy_list[x]: [Sub_df1['y'].max(),
+							Sub_df1['y'].max()]})
+	else:
+		# Get a list of the column headers
+		headers1 = df_tol1_ratios.columns.values.tolist()
+		# Check if the xdata is what is in the df_tol1 as the x_axis (if not no
+		# point plotting tolerances as all tolerances are vs this column).
+		max_x = Sub_df1['x'].max() + pd.DateOffset(weeks = 2)
+		min_x = Sub_df1['x'].min() + pd.DateOffset(weeks = -2)
+
+		for x in range(0, len(energy_list)):
+			if energy_list[x] in color_to_plot:
+				data.update({'x_' + energy_list[x]: [min_x, max_x],
+						'y_low_' + energy_list[x]:
+							[df_tol1_ratios['diff_ratios_' + energy_list[x]][0],
+							df_tol1_ratios['diff_ratios_' + energy_list[x]][0]],
+						'y_high_' + energy_list[x]:
+							[df_tol1_ratios['diff_ratios_' + energy_list[x]][1],
+							df_tol1_ratios['diff_ratios_' + energy_list[x]][1]]})
+			else:
+				data.update({'x_' + energy_list[x]: [Sub_df1['x'].max(),
+							Sub_df1['x'].max()],
+						'y_low_' + energy_list[x]: [Sub_df1['y'].max(),
+							Sub_df1['y'].max()],
+						'y_high_' + energy_list[x]: [Sub_df1['y'].max(),
+							Sub_df1['y'].max()]})
+
+	Sub_df1_tol1_ratios = pd.DataFrame(data)
+
+	return Sub_df1_tol1_ratios
+
+
+def Electron_Output_Graph(conn):
+
+	'''
+	Create a graph for the Electron Output table from the Photon database
+
+	This will also display quality index (2 depth method) results as these
+	are stored in the same table within the database.
+
+	'''
+
+	# Decide what the default viewing option is going to be.
+	x_data1 = 'adate'
+	y_data1 = 'graph % diff in output'
+	plot_title1 = 'Electron Output Results'
+	x_axis_title1 = x_data1
+	y_axis_title1 = y_data1
+	plot_size_height1 = 450
+	plot_size_width1 = 800
+	legend_location = 'bottom_left'
+	hover_tool_fields = ['chamber and electrometer', 'comments']
+	# Create a list of the plot parameters
+	list_plot_parameters = [x_data1, y_data1, plot_title1, x_axis_title1,
+		y_axis_title1, plot_size_height1, plot_size_width1, legend_location]
+	# Define the fields that the legend will be based off.
+	color_column = 'energy'
+	custom_color_boolean = False
+	custom_color_palette = ['#FF0000', 'black', 'yellow', 'purple', '#008F8F','#FF00FF', 'white']
+	marker_column = 'machinename'
+	custom_marker_boolean = False
+	custom_marker_palette = [ 	'circle_x', 'square', 'square_x', 'diamond',
+		'hex', 'x', 'circle_cross', 'square_cross', 'diamond_cross', 'dash',
+		'cross', 'inverted_triangle', 'circle', 'triangle', 'asterisk']
+	# From the legend defined above give the values that will be pre-ticked when
+	# the plot is opened.
+	color_to_plot = ['6MeV', '9MeV', '12MeV', '15MeV'] #may need to add two place values for colour elsewhere***********
+	marker_to_plot = ['TrueBeam B', 'TrueBeam C'] #removed one place value - may need amending in marker*****************
+
+	# Create a dataframe containing the data from the table
+	sql = 'SELECT [Protocol ID], [Energy], [chamber and electrometer], [Chamber factor], ' \
+		'[Temp], [Press], [T/P factor], [output], [Ratio: depth 2/depth 1], [Comments], ' \
+		'[Graph % Diff in output], [Graph abs diff in ratios] FROM [ecal_Graph] '
+	df = create_df(sql, conn)
+
+	# Create a list of the fields using the dataframe.
+	AxisFields = ['adate', 'temp', 'press', 't/p factor', 'output', 'graph % diff in output', 'ratio: depth 2/depth 1', 'graph abs diff in ratios']
+	TableFields = (list(df.columns))
+
+
+	# If you want to add/remove tolerances change the boolean to True/False
+	tolerance_boolean = True
+	# Create toleance dataframes.
+	# NB: df_tol1 is of 'normal' format. df_tol1_ratios is a special format that will
+	# interact with a custom function made in this file.
+	if tolerance_boolean == True:
+		df_tol1 = pd.DataFrame({'adate':[df['adate'].max(), df['adate'].max()],
+								'output':[98, 102],
+								'graph % diff in output':[-2, 2]})
+
+
+		df_tol1_ratios = pd.DataFrame({	'adate':[df['adate'].max(), df['adate'].max()],
+									'ratios_6MeV':[0.411, 0.571],
+									'ratios_9MeV':[0.432, 0.552],
+									'ratios_12MeV':[0.416, 0.496],
+									'ratios_15MeV':[0.465, 0.525],
+									'diff_ratios_6MeV':[-0.08, 0.08],
+									'diff_ratios_9MeV':[-0.06, 0.06],
+									'diff_ratios_12MeV':[-0.04, 0.04],
+									'diff_ratios_15MeV':[-0.03, 0.03]})
+
+	# Create columns for the legend
+	(color_list, color_palette, marker_list, marker_palette, df,
+		add_legend_to_df) = Create_Legend(df, color_column,
+		custom_color_boolean, custom_color_palette, marker_column,
+		custom_marker_boolean, custom_marker_palette
+		)
+
+	# Make a sub dataframe that will be plotted and convert to ColumnDataSource
+	Sub_df1 = Make_Dataset(df, color_column, color_to_plot, marker_column,
+		marker_to_plot, x_data1, y_data1)
+	src1 = ColumnDataSource(Sub_df1.to_dict(orient='list'))
+
+	# Create a plot
+	p1 = figure()
+	p1.scatter(	source = src1,
+				x = 'x',
+				y = 'y',
+				fill_alpha = 0.4,
+				size = 12,
+				legend_field = 'legend',
+				marker = factor_mark('marker1', marker_palette, marker_list),
+				color = factor_cmap('color1', color_palette, color_list)
+				)
+
+	# Set the plot parameters
+	Define_Plot_Parameters(p1, list_plot_parameters)
+
+	# Add tolerances if requested
+	if tolerance_boolean == True:
+
+		# Normal tolerances for outputs
+		Sub_df1_tol1 = Make_Dataset_Tolerance(x_data1, y_data1, Sub_df1, df_tol1)
+		src1_tol = ColumnDataSource(Sub_df1_tol1.to_dict(orient='list'))
+		# Add to plot
+		p1.line(source = src1_tol, x = 'x', y = 'y_low', color = 'firebrick')
+		p1.line(source = src1_tol, x = 'x', y = 'y_high', color = 'firebrick')
+
+		# Special tolerances to cope with quality index
+		Sub_df1_tol1_ratios = special_tolerance(color_to_plot, x_data1, y_data1,
+			Sub_df1, df_tol1_ratios)
+		src1_tol_ratios = ColumnDataSource(Sub_df1_tol1_ratios.to_dict(orient='list'))
+		# Add to plot
+		p1.line(source = src1_tol_ratios, x = 'x_6MeV', y = 'y_low_6MeV', color = 'yellow')
+		p1.line(source = src1_tol_ratios, x = 'x_6MeV', y = 'y_high_6MeV', color = 'yellow')
+		p1.line(source = src1_tol_ratios, x = 'x_9MeV', y = 'y_low_9MeV', color = 'mediumorchid')
+		p1.line(source = src1_tol_ratios, x = 'x_9MeV', y = 'y_high_9MeV', color = 'mediumorchid')
+		p1.line(source = src1_tol_ratios, x = 'x_12MeV', y = 'y_low_12MeV', color = 'firebrick')
+		p1.line(source = src1_tol_ratios, x = 'x_12MeV', y = 'y_high_12MeV', color = 'firebrick')
+		p1.line(source = src1_tol_ratios, x = 'x_15MeV', y = 'y_low_15MeV', color = 'black')
+		p1.line(source = src1_tol_ratios, x = 'x_15MeV', y = 'y_high_15MeV', color = 'black')
+
+	# Create a hovertool
+	hover1 = HoverTool()
+	# Check to make sure not too many fields are added to the field.
+	if len(hover_tool_fields) < 11:
+		kwargs = {}
+		i = 0
+		for x in hover_tool_fields:
+			i = i+1
+			kwargs['Field'+str(i)] = x
+	else:
+		kwargs = {}
+		msgbox('Too many fields selected to display on HoverTool ' \
+			'(Max = 10). Please reduce number of fields selected')
+	# Set hovertool parameters and add to the plot
+	Update_HoverTool(hover1, x_data1, y_data1, **kwargs)
+	p1.add_tools(hover1)
+
+
+ 	######## Add widgets
+	# Dropdown lists to change the x/y-axis.
+	select_xaxis, select_yaxis = Create_Select_Axis(AxisFields, x_axis_title1, y_axis_title1)
+	# Dropdown list to change the legend position.
+	select_legend = Create_Select_Legend(legend_location)
+	# Checkbox widgets used to create a tool to select the 'color' and 'marker' that are being plotted.
+	checkbox_color, checkbox_marker = Create_Checkbox_Legend(df, color_column,
+		color_to_plot, marker_column, marker_to_plot)
+	# Checkbox widget used to select hovertool fields
+	checkbox_hovertool = Create_Checkbox_HoverTool(TableFields, hover_tool_fields)
+	# Button to requery the database and get up to date data.
+	update_button = Button(label='Update', button_type='success')
+	# Button to set to a pre defined range instead of all data
+	range_button = Button(label='Range', button_type='primary')
+	# Titles for the checkboxes
+	color_title = Div(text='<b>Energy Choice</b>')
+	marker_title = Div(text='<b>Machine Choice</b>')
+	hover_title = Div(text='<b>Hovertool Fields</b>')
+
+	# Create a layout
+	if color_column == marker_column:
+		layout_checkbox = column([color_title, checkbox_color])
+	else:
+		layout_checkbox = column([color_title, checkbox_color, marker_title,
+			checkbox_marker])
+	button_row = row([update_button, range_button])
+	layout_plots = column([	button_row, select_xaxis, select_yaxis,
+							select_legend,p1])
+	tab_layout = row([layout_plots, layout_checkbox])
+
+
+	####################### CREATE CALLBACK FUNCTIONS ##########################
+	# Big callback that does most stuff
+	def callback(attr, old, new):
+
+		# Acquire the current values of all of the widgets
+		color_to_plot = [	checkbox_color.labels[i] for i in
+							checkbox_color.active]
+		if color_column != marker_column:
+			marker_to_plot = [checkbox_marker.labels[i] for i in
+								checkbox_marker.active]
+		else:
+			marker_to_plot = color_to_plot
+		hovertool_to_plot = [	checkbox_hovertool.labels[i] for i in
+								checkbox_hovertool.active]
+		plot1_xdata_to_plot = select_xaxis.value
+		plot1_ydata_to_plot = select_yaxis.value
+		legend_location = select_legend.value
+
+		# Set the new axis titles
+		x_axis_title1 = plot1_xdata_to_plot
+		y_axis_title1 = plot1_ydata_to_plot
+
+		# Create new version of the sub dataframe.
+		Sub_df1 = Make_Dataset(	df, color_column, color_to_plot, marker_column,
+			marker_to_plot, plot1_xdata_to_plot, plot1_ydata_to_plot)
+
+		# Update the plot.
+		Define_Plot_Parameters(p1, [plot1_xdata_to_plot, plot1_ydata_to_plot,
+	 		plot_title1, x_axis_title1, y_axis_title1, plot_size_height1,
+			plot_size_width1, legend_location])
+
+		# Update the hovertool
+		if len(hovertool_to_plot) < 11:
+			kwargs = {}
+			i = 0
+			for x in hovertool_to_plot:
+				i = i+1
+				kwargs['Field'+str(i)] = x
+		else:
+			kwargs = {}
+			msgbox('Too many fields selected to display on HoverTool ' \
+				'(Max = 10). Please reduce number of fields selected')
+		Update_HoverTool(hover1, plot1_xdata_to_plot, plot1_ydata_to_plot,
+			**kwargs)
+
+		# Update the tolerances.
+		if tolerance_boolean == True:
+			Sub_df1_tol1 = Make_Dataset_Tolerance(plot1_xdata_to_plot,
+				plot1_ydata_to_plot, Sub_df1, df_tol1)
+			Sub_df1_tol1_ratios = special_tolerance(color_to_plot,
+				plot1_xdata_to_plot, plot1_ydata_to_plot, Sub_df1, df_tol1_ratios)
+
+		# Update the ColumnDataSources.
+		src1.data = Sub_df1.to_dict(orient='list')
+		if tolerance_boolean == True:
+			src1_tol.data = Sub_df1_tol1.to_dict(orient='list')
+			src1_tol_ratios.data = Sub_df1_tol1_ratios.to_dict(orient='list')
+
+		return
+
+	select_xaxis.on_change('value', callback)
+	select_yaxis.on_change('value', callback)
+	select_legend.on_change('value', callback)
+	checkbox_color.on_change('active', callback)
+	checkbox_marker.on_change('active', callback)
+	checkbox_hovertool.on_change('active', callback)
+
+
+
+	# Callback for the Update Button
+	def callback_update():
+
+		# Make a new version of the dataframe going back to the database
+		df = create_df(sql, conn)
+		df = add_legend_to_df(df, color_column, marker_column)
+
+		color_to_plot = [checkbox_color.labels[i] for i in
+			checkbox_color.active]
+		if color_column != marker_column:
+			marker_to_plot = [checkbox_marker.labels[i] for i in
+				checkbox_marker.active]
+		else:
+			marker_to_plot = color_to_plot
+		hovertool_to_plot = [checkbox_hovertool.labels[i] for i in
+			checkbox_hovertool.active]
+		plot1_xdata_to_plot = select_xaxis.value
+		plot1_ydata_to_plot = select_yaxis.value
+		x_axis_title1 = plot1_xdata_to_plot
+		y_axis_title1 = plot1_ydata_to_plot
+		legend_location = select_legend.value
+
+		Sub_df1 = Make_Dataset(	df, color_column, color_to_plot, marker_column,
+			marker_to_plot, plot1_xdata_to_plot, plot1_ydata_to_plot)
+
+		Define_Plot_Parameters(p1, [plot1_xdata_to_plot, plot1_ydata_to_plot,
+	 		plot_title1, x_axis_title1, y_axis_title1, plot_size_height1,
+			plot_size_width1, legend_location])
+
+		if len(hovertool_to_plot) < 11:
+			kwargs = {}
+			i = 0
+			for x in hovertool_to_plot:
+				i = i+1
+				kwargs['Field'+str(i)] = x
+		else:
+			kwargs = {}
+			msgbox('Too many fields selected to display on HoverTool ' \
+				'(Max = 10). Please reduce number of fields selected')
+
+		Update_HoverTool(hover1, plot1_xdata_to_plot, plot1_ydata_to_plot,
+			**kwargs)
+
+		if tolerance_boolean == True:
+			Sub_df1_tol1 = Make_Dataset_Tolerance(plot1_xdata_to_plot,
+				plot1_ydata_to_plot, Sub_df1, df_tol1)
+			Sub_df1_tol1_ratios = special_tolerance(color_to_plot,
+				plot1_xdata_to_plot, plot1_ydata_to_plot, Sub_df1, df_tol1_ratios)
+
+		src1.data = Sub_df1.to_dict(orient='list')
+		if tolerance_boolean == True:
+			src1_tol.data = Sub_df1_tol1.to_dict(orient='list')
+			src1_tol_ratios.data = Sub_df1_tol1_ratios.to_dict(orient='list')
+
+		return
+
+	update_button.on_click(callback_update)
+
+
+
+	# Callback for the Range Button
+	# Sets reasonable range if certain fields are selected
+	def callback_range():
+
+		color_to_plot = [	checkbox_color.labels[i] for i in
+							checkbox_color.active]
+		if color_column != marker_column:
+			marker_to_plot = [	checkbox_marker.labels[i] for i in
+								checkbox_marker.active]
+		else:
+			marker_to_plot = color_to_plot
+		plot1_xdata_to_plot = select_xaxis.value
+		plot1_ydata_to_plot = select_yaxis.value
+
+		# Create new version of the sub-df
+		Sub_df1 = Make_Dataset(	df, color_column, color_to_plot, marker_column,
+			marker_to_plot, plot1_xdata_to_plot, plot1_ydata_to_plot)
+
+		if (plot1_xdata_to_plot == 'adate') and ((plot1_ydata_to_plot == 'graph % diff in output')
+			or (plot1_ydata_to_plot == 'output') or (plot1_ydata_to_plot =='ratio: depth 2/depth 1')
+			or (plot1_ydata_to_plot =='graph abs diff in ratios')):
+
+			p1.x_range.start = Sub_df1['x'].max() - timedelta(weeks=53)
+			p1.x_range.end = Sub_df1['x'].max() + timedelta(weeks=2)
+
+			if plot1_ydata_to_plot == 'output':
+				p1.y_range.start = 97
+				p1.y_range.end = 103
+			elif plot1_ydata_to_plot == 'graph % diff in output':
+				p1.y_range.start = -3
+				p1.y_range.end = 3
+			elif plot1_ydata_to_plot =='ratio: depth 2/depth 1':
+				p1.y_range.start = 0.35
+				p1.y_range.end = 0.65
+			elif plot1_ydata_to_plot =='graph abs diff in ratios':
+				p1.y_range.start = -0.15
+				p1.y_range.end = 0.15
+		return
+
+	range_button.on_click(callback_range)
+
+	# Return the panel to the main script
+	return Panel(child = tab_layout, title = 'Electron Output')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
